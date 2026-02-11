dda3fc65e0edc6059e844b7af9296ced4028dd86
Zakato12 <janjan8410@gmail.com>
2026-02-12 01:22:38 +0800

    For the Secure Web Architecture

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -3,6 +3,8 @@
 
 <title>@yield('title', 'Laravel System')</title>
 
+<link rel="icon" type="image/png" href="{{ asset('assets/apple-touch-icon.png') }}">
+
 <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
