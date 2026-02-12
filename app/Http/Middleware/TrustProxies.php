413495938bf3161675c2bc32fdc766873fd31f84
Zakato12 <janjan8410@gmail.com>
2026-02-12 15:08:47 +0800

    Revert some Research Gaps

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -19,24 +19,10 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
-
-    public function __construct()
-    {
-        $trustedProxies = trim((string) env('TRUSTED_PROXIES', ''));
-        $this->proxies = $trustedProxies === ''
-            ? null
-            : array_map('trim', explode(',', $trustedProxies));
-
-        $headerMode = strtoupper(trim((string) env('TRUSTED_PROXY_HEADERS', 'ALL')));
-        $this->headers = match ($headerMode) {
-            'AWS_ELB' => Request::HEADER_X_FORWARDED_AWS_ELB,
-            'FORWARDED' => Request::HEADER_FORWARDED,
-            default => Request::HEADER_X_FORWARDED_FOR
-                | Request::HEADER_X_FORWARDED_HOST
-                | Request::HEADER_X_FORWARDED_PORT
-                | Request::HEADER_X_FORWARDED_PROTO
-                | Request::HEADER_X_FORWARDED_AWS_ELB,
-        };
-    }
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
 }
