d81add419b1609885b660663e4bd74d0531352e5
Zakato12 <janjan8410@gmail.com>
2026-02-19 19:21:47 +0800

    Fix some error deploying

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -1,19 +1,5 @@
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
-
-<!-- Sidebar Toggle Script -->
-<!-- <script>
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleBtn = document.getElementById('sidebarToggle');
-    const wrapper = document.getElementById('wrapper');
-
-    if (toggleBtn && wrapper) {
-        toggleBtn.addEventListener('click', function () {
-            wrapper.classList.toggle('active');
-        });
-    }
-}); -->
 <script>
 document.addEventListener('DOMContentLoaded', function () {
     const toggleBtn = document.getElementById('sidebarToggle');
